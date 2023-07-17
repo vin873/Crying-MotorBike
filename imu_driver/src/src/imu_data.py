de00e01ba709eab7f9bea90cf173b9c1d06c335c
vin873 <109648831+vin873@users.noreply.github.com>
2023-07-17 20:37:26 +0800

    Update imu_data.py

--- imu_driver/src/src/imu_data.py
+++ imu_driver/src/src/imu_data.py
@@ -3,7 +3,7 @@
 import rospy
 from std_msgs.msg import Float64MultiArray
 
-def pry_callback(msg):
+def imu_callback(msg):
     print("inclination :", format(msg.data[0], '+.5f'))
     print("roll  :", format(msg.data[1], '+.5f'), "  pitch :", format(msg.data[2], '+.5f'), "  yaw   :", format(msg.data[3], '+.5f'))
     print("ang_X :", format(msg.data[4], '+.5f'), "  ang_Y :", format(msg.data[5], '+.5f'), "  ang_Z :", format(msg.data[6], '+.5f'))
@@ -12,7 +12,7 @@ def pry_callback(msg):
 
 def listener():
     rospy.init_node("imu_data")
-    rospy.Subscriber("imu_data", Float64MultiArray, pry_callback)
+    rospy.Subscriber("imu_data", Float64MultiArray, imu_callback)
     rospy.spin()
 
 if __name__=="__main__":

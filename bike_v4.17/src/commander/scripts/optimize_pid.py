d1a414bbe7e873eeaf1c3961c6d33d333f80a325
ArrangingFear56 <82667894+ArrangingFear56@users.noreply.github.com>
2023-07-18 23:20:18 +0800

    Add files via upload

---
+++ bike_v4.17/src/commander/scripts/optimize_pid.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+import rospy, time
+from deap import base, creator, tools
+import random
+import datetime
+from std_msgs.msg import Float64
+from gazebo_msgs.msg import LinkStates
+from geometry_msgs.msg import Pose, Twist
+from std_srvs.srv import Empty
+from sensor_msgs.msg import Imu
+import math
+from simple_pid import PID
+state = Pose()
+y_angle = 0
+y_angular=0
+y_angle_imu=0 
+y_angular_imu=0
+
+pub_back = rospy.Publisher('/back_fork_effort_controller/command', Float64, queue_size = 10)
+# pub_front = rospy.Publisher('/Revolute_5_position_controller/command', Float64, queue_size = 10)
+
+creator.create("FitnessMin", base.Fitness, weights=(1.0,))  
+creator.create("Individual", list, fitness=creator.FitnessMin)
+
+def euler_from_quaternion(x, y, z, w):
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+    return roll_x # in radians
+
+def get_pose(data):
+    global y_angle, state,y_angular
+    ind_pitch = data.name.index('new_wheel::base_link')
+    state = data.pose[ind_pitch]
+    y_angle = euler_from_quaternion(state.orientation.x, state.orientation.y, state.orientation.z, state.orientation.w)
+    state = data.twist[ind_pitch]
+    y_angular = state.angular.x
+
+def imucallback(msg):
+    global y_angle, y_angular, y_angle_imu, y_angular_imu
+    y_angle_imu = euler_from_quaternion(msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w)
+    y_angular_imu=msg.angular_velocity.z
+
+def gain_evaluation(individual):
+    global y_angle, pub_back, y_angular, y_angle_imu, y_angular_imu
+
+    Kp_p = individual[0]
+    Ki_p = individual[1]
+    Kd_p = individual[2]
+
+    time_interval = 0.001
+
+    rospy.wait_for_service('/gazebo/reset_simulation')
+    reset_simulation_client = rospy.ServiceProxy('/gazebo/reset_simulation',Empty) #rosservice call gazebo/pause_physics
+    reset_simulation_client()
+    y_angle = 0
+    y_angular=0
+    # max_angle=9999
+    # target_yaw_angle = 0
+    # last_error_pos = 0
+    # integral_yaw_error = 0
+    yaw_displacement_sum = 0
+    yaw_int=0
+    for i in range(5000):
+        time1 = time.time()
+        # pub_back.publish(pid(y_angle))
+        pub_back.publish(-(Kp_p*y_angle+Ki_p*yaw_int+Kd_p*y_angular))
+        # pub_back.publish(-Kp_p*y_angle_imu - Kd_p*y_angular_imu)
+        # pub_back.publish(effort_pos)
+        yaw_displacement_sum+=abs(y_angle)
+        yaw_int+=y_angle
+        time2 = time.time()
+        interval = time2 - time1
+        if abs(y_angle)>0.375:
+            # # integral_yaw_error = 0
+            # print("time:",i*time_interval+interval,f" individual : {individual}")
+            # # print("fitness:",yaw_displacement_sum,f" individual : {individual}")
+            # return 9999,
+            break
+    
+        if(interval < time_interval):
+            time.sleep(time_interval - interval)
+    
+    # integral_yaw_error = 0
+    # rospy.logwarn("==========================================")
+    # print("fitness:",yaw_displacement_sum,f" individual : {individual}")
+    print("fitness:",i*time_interval+interval,f" individual : {individual}")
+    # rospy.logwarn("==========================================")
+    return i*time_interval+interval,
+    # return yaw_displacement_sum,
+
+if __name__ == '__main__':
+    rospy.init_node('pid_gain_optimizer', anonymous=True)
+    rospy.Subscriber("/gazebo/link_states", LinkStates, get_pose)
+    rospy.Subscriber("/imu", Imu, imucallback)
+
+    print("#####genetic optimization#####")
+    toolbox = base.Toolbox()
+    toolbox.register("attr_gene", random.uniform , -15, 15)
+    # toolbox.register("attr_gene", random.gauss , 0, 1)
+    toolbox.register("evaluate", gain_evaluation)
+    toolbox.register("mate", tools.cxBlend, alpha=0.2)
+    # toolbox.register("mate", tools.cxTwoPoint)
+    # toolbox.register("mate", tools.cxPartialyMatched)
+    # toolbox.register("select", tools.selTournament, tournsize=7)
+    # toolbox.register("select", tools.selRoulette)
+    toolbox.register("select", tools.selStochasticUniversalSampling)
+
+    # GA parameters
+    N_GEN = 15        # number of generations
+    POP_SIZE = 100   # number of individuals
+    CX_PB = 0.5      # crossover probability
+    MUT_PB = 0.35     # mutation probability
+
+    random.seed(datetime.datetime.now())
+
+    points_number = 3
+    toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=0.5, indpb=0.2)
+    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_gene, points_number) #toolbox.attr_gene
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+    # creating population
+    pop = toolbox.population(n=POP_SIZE)
+
+    # evaluating each individual
+    fitnesses = list(map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = fit
+    print("  Evaluated %i individuals" % len(pop))
+
+    # fitness of each individual
+    fits = [ind.fitness.values[0] for ind in pop]
+
+    g = 0
+    while g < N_GEN:
+        g = g + 1
+        print("-- Generation %i --" % g)
+
+        offspring = toolbox.select(pop, len(pop))
+        offspring = list(map(toolbox.clone, offspring))
+
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+            if random.random() < CX_PB:
+                toolbox.mate(child1, child2)
+
+                del child1.fitness.values
+                del child2.fitness.values
+
+        for mutant in offspring:
+            if random.random() < MUT_PB:
+                toolbox.mutate(mutant)
+
+                del mutant.fitness.values
+
+        invalid_ind = []
+        for ind in offspring:
+            if not ind.fitness.valid:
+                invalid_ind.append(ind)
+
+        fitnesses = map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        pop[:] = offspring
+        
+        fits0 = [ind.fitness.values[0] for ind in pop]
+
+        length = len(pop)
+        mean = sum(fits0) / length
+        sum2 = sum(x * x for x in fits0)
+        std = abs(sum2 / length - mean**2)**0.5
+
+        print("  Min %s" % min(fits0))
+        print("  Max %s" % max(fits0))
+        print("  Avg %s" % mean)
+        print("  Std %s" % std)
+
+    # choosing the best individual
+    best_ind = tools.selBest(pop, 1)[0]
+    print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
---
+++ bike_v4.17/src/commander/scripts/run.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import rospy
+import time
+from std_msgs.msg import Float64
+from gazebo_msgs.msg import LinkStates
+from geometry_msgs.msg import Pose, Twist
+from sensor_msgs.msg import Imu
+import math
+from simple_pid import PID
+
+pole_twist = Twist()
+y_angle = 0
+y_angular=0
+y_angle_imu=0 
+y_angular_imu=0
+y_reference=0
+Kp_p = 11
+Kd_p = 0.4
+
+def euler_from_quaternion(x, y, z, w):
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+    ans = roll_x
+    return ans # in radians
+
+def imucallback(msg):
+    global y_angle, y_angular, y_angle_imu, y_angular_imu
+    y_angle_imu = euler_from_quaternion(msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w)
+    y_angular_imu=msg.angular_velocity.x
+    # rospy.loginfo("%f", data)
+
+def get_cart_pose(data):
+    global pole_twist, y_angle, y_angular
+    ind_pitch = data.name.index('new_wheel::base_link')
+    state = data.pose[ind_pitch]
+    y_angle = euler_from_quaternion(state.orientation.x, state.orientation.y, state.orientation.z, state.orientation.w)
+    state = data.twist[ind_pitch]
+    y_angular = state.angular.x
+
+def refCallback(msg):
+    global y_reference
+    y_reference=msg.data
+
+def KpCallback(msg):
+    global Kp_p
+    Kp_p=msg.data
+
+def KdCallback(msg):
+    global Kd_p
+    Kd_p=msg.data
+    
+def commander():
+    global pub_back, y_angular, y_angle, y_angle_imu, y_angular_imu, Kp_p, Kd_p
+    time_interval = 0.001
+    pub_back = rospy.Publisher('/back_fork_effort_controller/command', Float64, queue_size = 10)
+    y_angle=0
+    y_angular=0
+    lastD=0
+    while not rospy.is_shutdown():
+        time1 = time.time()
+        # print("ang:",y_angle,"imu_ang:",y_angle_imu,"   acc:",y_angular,"acc_imu:",y_angular_imu)
+        pub_back.publish(-(Kp_p*(y_angle) + Kd_p*y_angular))
+        # pub_back.publish(-(Kp_p*(y_angle-y_reference) + Kd_p*((y_angle-y_reference)-lastD)))
+        lastD=y_angle-y_reference
+        # pub_back.publish(-(Kp_p*y_angle_imu + Kd_p*y_angular_imu))
+        # pub_back.publish(pid(y_angle))
+        time2 = time.time()
+        interval = time2 - time1
+        if(interval < time_interval):
+            time.sleep(time_interval - interval)
+
+if __name__ == '__main__':
+    rospy.init_node('commander', anonymous=True)
+    rospy.Subscriber("/gazebo/link_states", LinkStates, get_cart_pose)
+    rospy.Subscriber("/imu", Imu, imucallback)
+    rospy.Subscriber("/refIn", Float64, refCallback)
+    rospy.Subscriber("/Kp", Float64, KpCallback)
+    rospy.Subscriber("/Kd", Float64, KdCallback)
+    commander()
+    rospy.spin()
